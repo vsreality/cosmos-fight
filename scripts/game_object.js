3729fa47f8e16dca1de64a153a7b56b96e6cced2
Richard Teammco <teammcr192@gmail.com>
2014-05-31 15:44:25 -0500

    Added a HealthManager module for GameObject. Not used by any objects yet.

--- scripts/game_object.js
+++ scripts/game_object.js
@@ -5,7 +5,7 @@
  * position of an object, and common functions necessary to use the object
  * in the Level subsystem.
  *
- * Main Object: GameObject()
+ * Main Object: GameObject() [abstract object]
  */
 
 
@@ -17,49 +17,6 @@ function GameObject() {
     this.y = 0;
     this.angle = ANGLE_UP;
     
-    
-    // Add a Collision Manager to this object:
-    this.hasCollisionMngr = false;
-    this.setCollisionManager = function(radius) {
-        this.collisionMngr = new CollisionManager(this);
-        this.collisionMngr.setCollision(radius);
-        this.hasCollisionMngr = true;
-    }
-    // Returns the Collision Manager of this object. If no Collision Manager
-    //	was set, returns a new Collision Manager for this object.
-    this.getCollisionManager = function() {
-        if(this.hasCollisionMngr)
-            return this.collisionMngr;
-        else
-            return new CollisionManager(this);
-    }
-    
-    
-    // Add a Motion Controller to this object:
-    this.hasMotionCtrl = false;
-    this.setMotionController = function(speed, angle) {
-        this.motionCtrl = new MotionController(this);
-        this.motionCtrl.setSpeed(speed);
-        this.motionCtrl.setAngle(angle);
-        this.hasMotionCtrl = true;
-    }
-    // Returns the Motion Controller of this object. If no Motion Controller
-    //  was set, returns a new Motion Controller for this object.
-    this.getMotionController = function() {
-        if(this.hasMotionCtrl)
-            return this.motionCtrl;
-        else
-            return new MotionController(this);
-    }
-    
-    
-    // Position getters:
-    this.getX = function() {
-        return this.x;
-    }
-    this.getY = function() {
-        return this.y;
-    }
     
     
     // Position setter: given an x, y position and (optionally) an angle.
@@ -81,32 +38,102 @@ function GameObject() {
         this.angle = angle;
     }
     
+    // Position getters:
+    this.getX = function() {
+        return this.x;
+    }
+    this.getY = function() {
+        return this.y;
+    }
+    
+    
+    
+    // Add a COLLISION MANAGER to this object:
+    this.hasCollisionMngr = false;
+    this.setCollisionManager = function(radius) {
+        this.collisionMngr = new CollisionManager(this);
+        this.collisionMngr.setCollision(radius);
+        this.hasCollisionMngr = true;
+    }
+    // Returns the Collision Manager of this object. If no Collision Manager
+    //	was set, returns a new Collision Manager for this object.
+    this.getCollisionManager = function() {
+        if(this.hasCollisionMngr)
+            return this.collisionMngr;
+        else
+            return new CollisionManager(this);
+    }
+    
+    
+    
+    // Add a MOTION CONTROLLER to this object:
+    this.hasMotionCtrl = false;
+    this.setMotionController = function(speed, angle) {
+        this.motionCtrl = new MotionController(this);
+        this.motionCtrl.setSpeed(speed);
+        this.motionCtrl.setAngle(angle);
+        this.hasMotionCtrl = true;
+    }
+    // Returns the Motion Controller of this object. If no Motion Controller
+    //  was set, returns a new Motion Controller for this object.
+    this.getMotionController = function() {
+        if(this.hasMotionCtrl)
+            return this.motionCtrl;
+        else
+            return new MotionController(this);
+    }
+    
+    
+    
+    // Add an EFFECTS MANAGER to this object:
+    
+    
+    
+    // Add a HEALTH MANAGER to this object:
+    this.hasHealthMngr = false;
+    this.setHealthManager = function(health) {
+        this.healthMngr = new HealthManager(this);
+        this.healthMngr.initHealth(health);
+        this.hasHealthMngr = true;
+    }
+    // Returns the Health Manager of this object. If no Health Manager
+    //  was set, returns a new Health Manager for this object.
+    this.getHealthManager = function() {
+        if(this.hasHealthMngr)
+            return this.healthMngr;
+        else
+            return new HealthManager(this);
+    }
+    
+    
+    
+    // Add an ATTACHMENT MANAGER to this object:
+    
+    
+    
+    // Add an ACTION CONTROLLER to this object:
+    
+    
+    
+    // Add a USER INPUT MANAGER to this object:
+    
+    
+    
+    // isAlive generally returns true as long as this object is active. The
+    //  object can be deactivated by setting alive to false.
+    // This function is overwritten by a HealthManager when set to return true
+    //  as long as the object's health is above 0.
+    this.alive = true;
+    this.isAlive = function() {
+        return this.alive;
+    }
+    
+    
     
     // Update function: update this object for this frame.
     this.update = function() { }
     
     // Draw function: animate this object on the screen.
     this.draw = function(ctx) { }
-    
-    
-    // Visual effects animations: TODO - put inside EffectsManager
-    this.hitEffect = function() { }
-    
-    
-    // Damage variables and functions: TODO - put inside another manager class?
-    this.health = 0;
-    this.applyDamage = function(damage) {
-        this.health -= damage;
-        if(this.health < 0)
-            this.health = 0;
-    }
-    
-    // isAlive is generally TRUE as long as this object is active and at some
-    //  amount of health in the game. It should return FALSE when the object
-    //  needs to be removed from the game.
-    this.alive = true;
-    this.isAlive = function() {
-        return this.alive;
-    }
     
 }
---
+++ scripts/health_manager.js
@@ -0,0 +1,97 @@
+/* File: health_manager.js
+ *
+ * A HealthManager is an object that can interface with any GameObject
+ * to track health and apply damage to the object.
+ *
+ * Main Object: HealthManager(gameObj)
+ */
+
+
+// HealthManager class
+// Parameter: gameObj to control, must be a GameObject interface.
+function HealthManager(gameObj) {
+
+    // keep track of the game object
+    this.gameObj = gameObj;
+    
+    // keep track of the object's current and maximum health
+    this.health = 0;
+    this.maxHealth = 0;
+    
+    
+    
+    // Sets the current and maximum health of the object at the given value,
+    //  and overwrites the object's isAlive function to return true when the
+    //  object's health is above 0. Given health should be larger than 0.
+    this.initHealth = function(health) {
+        this.health = health;
+        this.maxHealth = health;
+        
+        this.gameObj.isAlive = function() {
+            return this.getHealthManager().getHealth() > 0;
+        }
+    }
+    
+    // Updates the object's maximum health. If the current health is larger
+    //  than the maximum health, it will be reduced accordingly.
+    this.setMaxHealth = function(maxHealth) {
+        this.maxHealth = maxHealth;
+        
+        if(this.health > this.maxHealth)
+            this.health = this.maxHealth;
+    }
+    
+    // Updates the object's current health to the given value. If new health
+    //  amount is less than 0 or greater than maxHealth, it will be cut off
+    //  accordingly.
+    this.setHealth = function(health) {
+        this.health = health;
+        
+        if(this.health > this.maxHealth)
+            this.health = this.maxHealth;
+        if(this.health < 0)
+            this.health = 0;
+    }
+    
+    
+    
+    // Returns this object's current health.
+    this.getHealth = function() {
+        return this.health;
+    }
+    
+    // Returns this object's maximum health value.
+    this.getMaxHealth = function() {
+        return this.maxHealth;
+    }
+    
+    // Returns the decimal representation of the object's health percentage.
+    // Value returned is between 0 and 1: 0 is no health, 1 is max health.
+    // NOTE: if maxHealth <= 0 (it shouldn't be!), this function returns 0.
+    this.getHealthPercent = function() {
+        if(this.maxHealth > 0)
+            return this.health / this.maxHealth;
+        else
+            return 0;
+    }
+    
+    
+    
+    // Apply the given amount of damage to this object: reduce its health
+    //  by that amount. If health goes below 0, it is cut off at 0.
+    // Parameters: amount should be a positive value.
+    this.applyDamage = function(amount) {
+        this.health -= amount;
+        if(this.health < 0)
+            this.health = 0;
+    }
+    
+    // Heal the object by the given amount of points: increase its health by
+    //  that amount. If health goes above maxHealth, it is cut off at maxHealth.
+    // Parameters: amount should be a positive value.
+    this.applyHeal = function(amount) {
+        this.health += amount;
+        if(this.health > this.maxHealth)
+            this.health = this.maxHealth;
+    }
+}
